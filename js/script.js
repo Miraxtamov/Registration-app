a1dc39557d6c9d31941a8d7d2235be8704428336
Miraxtamov <jmiraxtamov@gmail.com>
2022-03-29 18:03:04 +0500

    Add search design

--- js/script.js
+++ js/script.js
@@ -30,31 +30,13 @@ function addTable() {
 	document.querySelector("#table").innerHTML += table;
 }
 
+
 form.addEventListener("submit", (e) => {
 	e.preventDefault();
 
 	input.value = null;
 	lastName.value = null;
 	userAge.value = null;
-
-	let newObj = {
-		id: list.length + 1,
-		firstName: firstName.value,
-		lastName: lastName.value,
-		age: userAge.value,
-		category: category.value,
-	};
-	list.push(newObj);
-	render(list);
-
-	let list;
-
-if (localStorage.getItem("info")) {
-	list = JSON.parse(localStorage.getItem("info"));
-} else {
-	list = [];
-}
-
-	localStorage.setItem("info", JSON.stringify(newObj));
-	input.forEach((i) => (i.value = ""));
 });
+
+
